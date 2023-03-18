d8e4724b1eed57479a739982892d10a9019be091
AndriyNich <info@bembi.ua>
2023-03-18 21:41:49 +0200

    Create Profile.jsx

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types';
+import {
+  Card,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  Stats,
+  StatsItem,
+  Label,
+  Quantity,
+} from './Profile.styled';
+
+//
+export const Profile = ({ userdata }) => {
+  const {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  } = userdata;
+  return (
+    <Card>
+      <Description>
+        <Avatar src={avatar} alt={username} />
+        <Name>{username}</Name>
+        <Tag>&#64;{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+      <Stats>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>
+            {new Intl.NumberFormat('en-IN').format(followers)}
+          </Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{new Intl.NumberFormat('en-IN').format(views)}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{new Intl.NumberFormat('en-IN').format(likes)}</Quantity>
+        </StatsItem>
+      </Stats>
+    </Card>
+  );
+};
+
+Profile.propTypes = {
+  userdata: PropTypes.shape({
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    stats: PropTypes.shape({
+      followers: PropTypes.number,
+      views: PropTypes.number,
+      likes: PropTypes.number,
+    }),
+  }),
+};
