5f245d0ce175f726972f70057196c7aaf4dce86c
AndriyNich <info@bembi.ua>
2023-03-18 21:34:08 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import user from '../data/user.json';
 import data from '../data/data.json';
 import friendList from '../data/friends.json';
 import transactions from '../data/transactions.json';
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { Profile } from './Profile/Profile.jsx';
+import { Statistics } from './Statistics/Statistics.jsx';
+import { FriendList } from './FriendList/FriendList.jsx';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
 
 export const App = () => {
   return (
